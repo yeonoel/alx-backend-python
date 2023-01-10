7652227a5dae82f4b93f894f62142ea43242828f
yeonoel <yeopevroguinoel@gmail.com>
2023-01-10 14:40:10 +0000

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/en python3
+"""Define variables."""
+
+
+from xmlrpc.client import boolean
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
+school: str = "Holberton"
