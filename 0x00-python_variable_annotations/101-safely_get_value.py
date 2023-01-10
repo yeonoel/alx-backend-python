8ca388756961682ede33629e9fbd2d06d4a4da9b
yeonoel <yeopevroguinoel@gmail.com>
2023-01-10 16:30:26 +0000

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,6 +11,8 @@ def safely_get_value(
         key: Any,
         default: Union[T, None] = None
         ) -> Union[Any, T]:
+    """Given the parameters and the return values,
+        add type annotations to the function."""
     if key in dct:
         return dct[key]
     else:
